4455ca612d6d669d1e042a22600f889fe3a002b7
John <johnzhengyuliu@gmail.com>
2024-06-03 13:48:11 -0500

    change file structure

--- pages/learn/_meta.ts
+++ pages/learn/_meta.ts
@@ -1,9 +1,21 @@
 export default {
   index: {
     title: "Solana Playground",
+    display: "hidden",
   },
-  tutorial: {
-    title: "Quick Start",
+  overview: {
+    title: "Overview",
+    // display: "hidden",
+  },
+  client: {
+    title: "Client",
+  },
+  program: {
+    title: "Program",
+    // display: "hidden",
+  },
+  frontend: {
+    title: "Frontend",
     // display: "hidden",
   },
 };
